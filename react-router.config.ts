b7f51132bc0d1bbb7a27d526784ae1fc27fe803f
Sepehr Tavanaiee <iamsrte.pers@gmail.com>
2025-09-14 16:10:55 +0330

    fix routing problem

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,5 @@ import type { Config } from '@react-router/dev/config';
 export default {
   ssr: false,
   prerender: true,
+  basename: "/adroam/"
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    base: mode === 'production' ? '/adroam/' : '/',
-  };
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  base: '/adroam/',
 });
